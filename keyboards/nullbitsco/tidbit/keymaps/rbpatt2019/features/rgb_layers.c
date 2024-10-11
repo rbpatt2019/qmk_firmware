9379b738a83ce4b990a7d6100b2d4be867b6027e
rbpatt2019 <rb.patterson.cross@gmail.com>
2024-10-11 10:48:29 +0100

    fix(tidbit): correct layering for lights

--- keyboards/nullbitsco/tidbit/keymaps/rbpatt2019/features/rgb_layers.c
+++ keyboards/nullbitsco/tidbit/keymaps/rbpatt2019/features/rgb_layers.c
@@ -1,11 +1,11 @@
 const rgblight_segment_t PROGMEM volume[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 8, HSV_BLUE } );
-const rgblight_segment_t PROGMEM brightness[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 4, HSV_CYAN } );
 const rgblight_segment_t PROGMEM app_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 4, 4, HSV_GREEN } );
+const rgblight_segment_t PROGMEM brightness[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 4, HSV_CYAN } );
 const rgblight_segment_t PROGMEM chrome_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 4, 4, HSV_MAGENTA } );
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     volume,
-    brightness,
     app_layer,
+    brightness,
     chrome_layer
 );
 
@@ -19,8 +19,8 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(1, layer_state_cmp(state, 1));  //bright
-    rgblight_set_layer_state(2, layer_state_cmp(state, 2));  //apps
+    rgblight_set_layer_state(1, layer_state_cmp(state, 1));  //apps
+    rgblight_set_layer_state(2, layer_state_cmp(state, 2));  //bright
     rgblight_set_layer_state(3, layer_state_cmp(state, 3));  //chrome
     return state;
 }
